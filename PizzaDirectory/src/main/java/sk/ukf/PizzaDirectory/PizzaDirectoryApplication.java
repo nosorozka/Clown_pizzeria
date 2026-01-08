6c013ae64fac72cf428ebac559b4df203cef7bd2
Sheiko Yehor <nosorozka98@gmail.com>
2026-01-08 21:44:54 +0100

    new folder

---
+++ PizzaDirectory/src/main/java/sk/ukf/PizzaDirectory/PizzaDirectoryApplication.java
@@ -0,0 +1,13 @@
+package sk.ukf.PizzaDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaDirectoryApplication.class, args);
+    }
+}
+
---
+++ PizzaDirectory/src/test/java/sk/ukf/PizzaDirectory/PizzaDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.ukf.PizzaDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaDirectoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
